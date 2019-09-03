9c0eb3e8ac863d50b784fc59ba3cf6853727e770
Jaehyeok13 <star76855@naver.com>
2019-09-03 15:12:12 +0900

    123

--- src/main/java/com/junwo/ockdong/admin/controller/AdminController.java
+++ src/main/java/com/junwo/ockdong/admin/controller/AdminController.java
@@ -22,5 +22,11 @@ public class AdminController {
 	public String productList() {
 		return "admin/products/productList";
 	}
+	
+	// 관리자에서 로고 누를시 처음 화면으로 이동
+	@RequestMapping("adminhome.do")
+	public String adminHome() {
+		return "admin/adminMain";
+	}
 
 }
