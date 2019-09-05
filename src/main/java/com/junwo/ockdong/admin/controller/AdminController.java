d37d634829999a23fb828da6dfef0e244aba2327
choikc <ckc5749@gmail.com>
2019-09-05 17:43:34 +0900

    관리자 수정
    관리자 수정

--- src/main/java/com/junwo/ockdong/admin/controller/AdminController.java
+++ src/main/java/com/junwo/ockdong/admin/controller/AdminController.java
@@ -1,32 +1,33 @@
 package com.junwo.ockdong.admin.controller;
 
-import java.sql.Date;
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
+import org.springframework.web.servlet.ModelAndView;
 
 import com.junwo.ockdong.member.model.service.MemberService;
 import com.junwo.ockdong.member.model.vo.Member;
+import com.junwo.ockdong.myOwn.model.service.MyOwnService;
 import com.junwo.ockdong.myOwn.model.vo.Ingredient;
-
 
 @Controller
 public class AdminController {
 	@Autowired
 	private MemberService mService;
-	
+
+	@Autowired
+	private MyOwnService moService;
+
 	@RequestMapping("adminView.do")
 	public String adminView() {
 
@@ -41,31 +42,30 @@ public class AdminController {
 
 	@RequestMapping("productList.do")
 	public String productList() {
-		
-		
+
 		return "admin/products/productList";
 	}
-	
+
 	// 관리자에서 로고 누를시 처음 화면으로 이동
 	@RequestMapping("adminhome.do")
 	public String adminHome() {
 		return "admin/adminMain";
 	}
-	
-	//결제 통합리스트
-	
+
+	// 결제 통합리스트
+
 	@RequestMapping("PayResultList.do")
 	public String PayResultList() {
-		
-		return"admin/Payment/adminResultList";
+
+		return "admin/Payment/adminResultList";
 	}
-	
+
 	@RequestMapping("adminWarnning.do")
 	public String Warnninng() {
-		
-		return"admin/Payment/adminWarnningList";
+
+		return "admin/Payment/adminWarnningList";
 	}
-	
+
 	// 나만의 도시락 관리 페이지
 	@RequestMapping("mydo.do")
 	public String mydoView() {
@@ -74,56 +74,63 @@ public class AdminController {
 
 	// 나만의 도시락 재료관리
 	@RequestMapping("myIn.do")
-	public String myInView() {
-		return "admin/mydo/myOwn_Ingredients";
+	public ModelAndView myInView(ModelAndView mv) {
+		
+		ArrayList<Ingredient> inList = moService.selectAll();
+		
+		if(inList != null) {
+			mv.addObject("inList", inList);
+			mv.setViewName("admin/mydo/myOwn_Ingredients");
+		}else {
+			mv.setViewName("admin/adminMain");
+		}
+		return mv;
 	}
-	
+
 	// 나만의 도시락 재료 등록 페이지 이동
 	@RequestMapping("IngredientsInsert.do")
 	public String IngredientsInsertView() {
 		return "admin/mydo/myOwn_Ingredients_Insert";
 	}
-	
+
 	// 나만의 도시락 재료 등록
 	@RequestMapping("Ingredients_Insert.do")
-	public String Ingredients_Insert(@ModelAttribute Ingredient in,
-									 @RequestParam("inCategory") String inCategory,
-									 @RequestParam("selected4") String selected4,
-									 @RequestParam("selected5") String selected5,
-									 @RequestParam(value="ingredientImg", required = false) MultipartFile uploadFile,
-									 HttpServletRequest request) {
-		
+	public String Ingredients_Insert(@ModelAttribute Ingredient in, @RequestParam("inCategory") String inCategory,
+			@RequestParam("selected4") String selected4, @RequestParam("selected5") String selected5,
+			@RequestParam(value = "ingredientImg", required = false) MultipartFile uploadFile,
+			HttpServletRequest request) {
+
 		System.out.println(in);
-		
-		if(inCategory.equals("4찬")) {
+
+		if (inCategory.equals("4찬")) {
 			in.setInType(selected4);
 			inCategory = inCategory + "\\" + selected4;
-		}else {
+		} else {
 			in.setInType(selected5);
 			inCategory = inCategory + "\\" + selected5;
 		}
-		
-		if(uploadFile != null && !uploadFile.isEmpty()) {
+
+		if (uploadFile != null && !uploadFile.isEmpty()) {
 			// 저장할 경로를 지정하는 saveFile()메소드 생성
-			
+
 			String renameFileName = inSaveFile(uploadFile, request, inCategory);
-			
-			if(renameFileName != null) {
+
+			if (renameFileName != null) {
 				in.setInOriginalFile(uploadFile.getOriginalFilename());
 				in.setInRenameFile(renameFileName);
 			}
 		}
-		
+
 		System.out.println(in);
-		
+
 		int result = moService.insert(in);
-		
-		if(result > 0) {
+
+		if (result > 0) {
 			return "redirect:myIn.do";
-		}else {
+		} else {
 			return "redirect:IngredientsInsert.do";
 		}
-		
+
 	}
 
 	// 저장할 파일의 이름 만들기? + 확장자 명도 추가
@@ -153,110 +160,97 @@ public class AdminController {
 
 		return renameFile;
 	}
-	
+
 	// 상품관리 상품등록
 	@RequestMapping("productsInsert.do")
 	public String productsInert() {
 		return "admin/products/productInsert";
 	}
-	
+
 	// 상품 문의
 	@RequestMapping("productInquiry.do")
 	public String productInquiry() {
 		return "admin/products/productInquiry";
 	}
-	
+
 	// 회원리스트~~
-		@RequestMapping("adminMemberList.do")
-	public ModelAndView adminMemberList(@RequestParam(value="page",required=false)ModelAndView mv)  {	
-		mv = new ModelAndView(); 
-	
-		
+	@RequestMapping("adminMemberList.do")
+	public ModelAndView adminMemberList(@RequestParam(value = "page", required = false) ModelAndView mv) {
+		mv = new ModelAndView();
+
 		ArrayList<Member> list = mService.SelectMemberList();
-		
-		if(list !=null) {
-			mv.addObject("list",list);
+
+		if (list != null) {
+			mv.addObject("list", list);
 			mv.setViewName("admin/Member/adminMemberList");
-			 	
-		}else {
-			
-		
-			
+
+		} else {
+
 		}
-		
+
 		return mv;
-		
-		
+
 	}
-	
-	
-	
+
 	@RequestMapping("adminMemberList2.do")
-	public ModelAndView adminMemberList2(@RequestParam(value="page",required=false)ModelAndView mv,@RequestParam("searchInput")String searchInput
-			,@RequestParam("searchForm")String searchForm,@RequestParam("startDatePicker")String startDatePicker,String endDatePicker)  {	
-		mv = new ModelAndView(); 
+	public ModelAndView adminMemberList2(@RequestParam(value = "page", required = false) ModelAndView mv,
+			@RequestParam("searchInput") String searchInput, @RequestParam("searchForm") String searchForm,
+			@RequestParam("startDatePicker") String startDatePicker, String endDatePicker) {
+		mv = new ModelAndView();
 		System.out.println(searchInput);
 		System.out.println(searchForm);
 		System.out.println(endDatePicker);
 		System.out.println(startDatePicker);
-		
-		
-		HashMap<String,String> search = new HashMap<String,String>();
-		search.put("searchInput",searchInput);
-		search.put("searchForm",searchForm);
-		search.put("startDatePicker",startDatePicker);
-		search.put("endDatePicker",endDatePicker);
+
+		HashMap<String, String> search = new HashMap<String, String>();
+		search.put("searchInput", searchInput);
+		search.put("searchForm", searchForm);
+		search.put("startDatePicker", startDatePicker);
+		search.put("endDatePicker", endDatePicker);
 		ArrayList<Member> list = mService.SelectMemberList2(search);
 //		int listCount = mService.MemberListCount();
 //		System.out.println(list);
-		if(list !=null) {
-			mv.addObject("list",list);
+		if (list != null) {
+			mv.addObject("list", list);
 			mv.setViewName("admin/Member/adminMemberList");
-			 	
-		}else {
-			
-		
-			
+
+		} else {
+
 		}
-		
+
 		return mv;
-		
-		
+
 	}
-	
-	//탈퇴 회원 리스트~~
-	
+
+	// 탈퇴 회원 리스트~~
 
 	@RequestMapping("adminSecessionList.do")
-	public ModelAndView adminSecession(@RequestParam(value="page",required=false)ModelAndView mv,@RequestParam("searchInput")String searchInput
-			,@RequestParam("searchForm")String searchForm,@RequestParam("startDatePicker")String startDatePicker,String endDatePicker)  {	
-		mv = new ModelAndView(); 
+	public ModelAndView adminSecession(@RequestParam(value = "page", required = false) ModelAndView mv,
+			@RequestParam("searchInput") String searchInput, @RequestParam("searchForm") String searchForm,
+			@RequestParam("startDatePicker") String startDatePicker, String endDatePicker) {
+		mv = new ModelAndView();
 		System.out.println(searchInput);
 		System.out.println(searchForm);
 		System.out.println(endDatePicker);
 		System.out.println(startDatePicker);
-		
-		
-		HashMap<String,String> search = new HashMap<String,String>();
-		search.put("searchInput",searchInput);
-		search.put("searchForm",searchForm);
-		search.put("startDatePicker",startDatePicker);
-		search.put("endDatePicker",endDatePicker);
+
+		HashMap<String, String> search = new HashMap<String, String>();
+		search.put("searchInput", searchInput);
+		search.put("searchForm", searchForm);
+		search.put("startDatePicker", startDatePicker);
+		search.put("endDatePicker", endDatePicker);
 		ArrayList<Member> list = mService.SelectMemberList2(search);
 //		int listCount = mService.MemberListCount();
 //		System.out.println(list);
-		if(list !=null) {
-			mv.addObject("list",list);
+		if (list != null) {
+			mv.addObject("list", list);
 			mv.setViewName("admin/Member/adminMemberSecession");
-			 	
-		}else {
-			
-		
-			
+
+		} else {
+
 		}
-		
+
 		return mv;
 	}
-
 
 }
